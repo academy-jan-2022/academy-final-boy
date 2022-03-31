78e7aeb2c429587900c80656dcfc78a140bae562
danihra-codurance <daniel.rodriguez@codurance.com>
2022-03-31 13:53:28 +0100

    Test containers integration

--- src/test/java/codurance/academyfinalboy/backend/BackendApplicationTests.java
+++ src/test/java/codurance/academyfinalboy/backend/BackendApplicationTests.java
@@ -2,10 +2,23 @@ package codurance.academyfinalboy.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.File;
 
 @SpringBootTest
+@Testcontainers
 class BackendApplicationTests {
 
-  @Test
-  void contextLoads() {}
+    @Container
+    public static DockerComposeContainer environment =
+            new DockerComposeContainer(new File("docker-compose.yaml"))
+                    .withExposedService("db", 1433);
+
+
+    @Test
+    void contextLoads() {
+    }
 }
