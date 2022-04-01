849a03e828388c30b507489de118c2196cca4a29
danihra-codurance <daniel.rodriguez@codurance.com>
2022-04-01 14:36:29 +0100

    Add cucumber to tests

--- src/test/java/cucumber/HeartbeatStepDefs.java
+++ src/test/java/cucumber/HeartbeatStepDefs.java
@@ -1,18 +1,35 @@
 package cucumber;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+import org.springframework.boot.web.server.LocalServerPort;
 
-public class HeartbeatStepDefs extends SpringContextConfiguration {
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "cucumber")
+public class HeartbeatStepDefs  {
+
+  @LocalServerPort
+  private int port = 0;
 
   private Response response;
 
   @When("Heartbeat api is called")
   public void heartbeatApiIsCalled() {
+    System.out.println("se lanzaron los tests = ");
     response = given().port(port).when().get("/actuator/health");
   }
 
@@ -20,4 +37,5 @@ public class HeartbeatStepDefs extends SpringContextConfiguration {
   public void theStatusIsUp() {
     response.then().statusCode(200).body("status", equalTo("UP"));
   }
+
 }
---
+++ src/test/resources/features/featurazo.feature
@@ -0,0 +1,5 @@
+Feature: Featurazo
+
+  Scenario: scenario distinto
+    When Heartbeat api is called
+    Then the status is up
