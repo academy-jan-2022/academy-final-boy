1134331565c2f847af9f6890ef314373a256f212
danihra-codurance <daniel.rodriguez@codurance.com>
2022-04-06 10:32:00 +0100

    Cleanup properties

--- src/test/java/codurance/academyfinalboy/backend/BackendApplicationTests.java
+++ src/test/java/codurance/academyfinalboy/backend/BackendApplicationTests.java
@@ -1,6 +1,5 @@
 package codurance.academyfinalboy.backend;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
