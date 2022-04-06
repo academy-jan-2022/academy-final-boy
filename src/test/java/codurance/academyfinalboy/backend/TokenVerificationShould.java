29fb7e13a47c88c6385939569211286b9c3588ff
Nicola <nicola_rushton@outlook.com>
2022-04-06 16:25:02 +0100

    refactor

---
+++ src/test/java/codurance/academyfinalboy/backend/TokenVerificationShould.java
@@ -0,0 +1,28 @@
+package codurance.academyfinalboy.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(TokenVerificationController.class)
+public class TokenVerificationShould {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void unauthorized_calls_without_a_valid_token_return_a_401_error() throws Exception {
+        // Given
+        String token = "";
+
+        mockMvc.perform(post("/tokenvalidator")
+                        .content(token)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized());
+    }
+}
