dd43a018080c78a98930600762e235b3a936c2c9
danihra-codurance <daniel.rodriguez@codurance.com>
2022-04-06 10:29:27 +0100

    Setup makefile

--- Makefile
+++ Makefile
@@ -1,17 +1,16 @@
-
-up:
-	@docker compose up -d
+run-dev:
+	@docker compose --env-file .env.dev up -d
 
 down:
-	docker compose down
+	docker compose --env-file .env.dev down
 
 build-backend:
 	docker build -t academy/teaminator-back .
 
 start-db:
-	docker compose up -d db
+	docker compose --env-file .env.test up -d db
 
 test:
 	./gradlew test
 
-run-test: up test down
+run-test: start-db test down
--- src/test/java/cucumber/SpringContextConfiguration.java
+++ src/test/java/cucumber/SpringContextConfiguration.java
@@ -3,9 +3,11 @@ package cucumber;
 import codurance.academyfinalboy.backend.BackendApplication;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(
     classes = BackendApplication.class,
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @CucumberContextConfiguration
+@ActiveProfiles("test")
 class SpringContextConfiguration {}
