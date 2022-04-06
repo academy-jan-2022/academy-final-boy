f7933ed18811a7a931c4145c9a54d818c03e7e26
Nicola <nicola_rushton@outlook.com>
2022-04-06 15:56:55 +0100

    Formatting

--- src/test/java/codurance/academyfinalboy/backend/VerifyTokenEndopint.java
+++ src/test/java/codurance/academyfinalboy/backend/VerifyTokenEndopint.java
@@ -1,5 +1,6 @@
 package codurance.academyfinalboy.backend;
 
+import java.io.IOException;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpPost;
@@ -8,17 +9,15 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-
 class TokenVerificationEndpointShould {
 
   @Test
-  void unauthorized_calls_without_a_valid_token() throws IOException {
-    //Given
+  void unauthorized_calls_without_a_valid_token_return_a_401_error() throws IOException {
+    // Given
     HttpUriRequest request = new HttpPost("http://localhost:8080/actuator/tokenvalidator");
-    //When
+    // When
     HttpResponse response = HttpClientBuilder.create().build().execute(request);
-    //THEN
+    // THEN
     Assertions.assertEquals(HttpStatus.SC_UNAUTHORIZED, response.getStatusLine().getStatusCode());
   }
 }
