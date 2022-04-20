1592edbaf2945cc0065cf0accce895a13dcbe598
danihra-codurance <97448011+danihra-codurance@users.noreply.github.com>
2022-04-20 11:02:58 +0200

    Monitoring (#15)
    * wip add key
    
    * add telemetry
    
    * add controller advice
    
    * remove telemetry from successful request
    
    * rename env variable for insights ke

--- Makefile
+++ Makefile
@@ -4,11 +4,14 @@ run-dev:
 down:
 	docker compose --env-file .env.dev down
 
-build-backend:
+build-backend: format
 	docker build -t academy/teaminator-back .
 
 start-db:
 	docker compose --env-file .env.test up -d db
+
+format:
+	./gradlew :spotlessApply
 
 test:
 	./gradlew test
--- src/test/java/codurance/academyfinalboy/backend/web/controllers/LoginControllerShould.java
+++ src/test/java/codurance/academyfinalboy/backend/web/controllers/LoginControllerShould.java
@@ -1,14 +1,18 @@
 package codurance.academyfinalboy.backend.web.controllers;
 
 import static codurance.academyfinalboy.backend.web.controllers.LoginController.LoginRequest;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import codurance.academyfinalboy.backend.BaseSpringTest;
 import codurance.academyfinalboy.backend.actions.Login;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsoft.applicationinsights.TelemetryClient;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -20,6 +24,7 @@ class LoginControllerShould extends BaseSpringTest {
   @Autowired ObjectMapper mapper;
 
   @MockBean Login login;
+  @MockBean TelemetryClient telemetryClient;
 
   @Test
   void call_login_action() throws Exception {
@@ -33,5 +38,22 @@ class LoginControllerShould extends BaseSpringTest {
         .andExpect(status().isOk());
 
     verify(login).execute(loginRequest.externalId(), loginRequest.fullName());
+  }
+
+  @Test
+  void publish_exception_on_error() throws Exception {
+    var loginRequest = new LoginRequest("1561560156610", "fullName");
+    IllegalStateException exception = new IllegalStateException("tested exception");
+    doThrow(exception).when(login).execute(anyString(), anyString());
+
+    mockMvc
+        .perform(
+            post("/login")
+                .contentType(APPLICATION_JSON)
+                .content(mapper.writeValueAsString(loginRequest)))
+        .andExpect(status().is4xxClientError())
+        .andExpect(jsonPath("$.message").value(exception.getMessage()));
+
+    verify(telemetryClient).trackException(exception);
   }
 }
