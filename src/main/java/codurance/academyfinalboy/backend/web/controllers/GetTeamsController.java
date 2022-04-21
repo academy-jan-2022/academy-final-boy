2550e263bb08e30d62cbf4edce68a365a0929301
Simón Fernández Besteiro <simonbesteiro@gmail.com>
2022-04-21 11:19:08 +0200

    [ADD] Get team feature
    * add Authorized user
    
    * configure BaseSpringTest
    
    * cucumber test
    
    * create step defs
    
    * spotless appply
    
    * add team
    
    * Create team endpoint and start action
    
    * green
    
    * red
    
    * green
    
    * green
    
    * red
    
    * green
    
    * fix cucumber test skind of
    
    * Add teams to user
    
    * Implement team service
    
    * Save teams
    
    * get team by id
    
    * proper red
    
    * proper red
    
    * green
    
    * red
    
    * red
    
    * green
    
    * red
    
    * green
    
    * green
    
    * red
    
    * green
    
    * refactor
    
    * proper red
    
    * green
    
    * spotless
    
    * broken acceptance test
    
    * green acceptance
    
    * refactor
    
    * green
    
    * green
    
    * spotless
    
    * remove unused class
    
    * modify body return when get teams
    
    * name change
    
    Co-authored-by: danihra-codurance <daniel.rodriguez@codurance.com>
    Co-authored-by: Brian Hoang <brian.hd.hoang@codurance.com>
    Co-authored-by: iagardi <garbigz@gmail.com>
    Co-authored-by: Nicola <nicola_rushton@outlook.com>

---
+++ src/main/java/codurance/academyfinalboy/backend/web/controllers/GetTeamsController.java
@@ -0,0 +1,24 @@
+package codurance.academyfinalboy.backend.web.controllers;
+
+import codurance.academyfinalboy.backend.actions.GetTeams;
+import codurance.academyfinalboy.backend.model.team.Team;
+import java.util.List;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class GetTeamsController {
+
+  private GetTeams getTeams;
+
+  public GetTeamsController(GetTeams getTeams) {
+    this.getTeams = getTeams;
+  }
+
+  @GetMapping("/teams")
+  public GetTeamsResponse getTeams() {
+
+    return new GetTeamsResponse(getTeams.execute());
+  }
+
+  public record GetTeamsResponse(List<Team> teams) {}
+}
---
+++ src/test/java/codurance/academyfinalboy/backend/web/controllers/GetTeamsControllerShould.java
@@ -0,0 +1,56 @@
+package codurance.academyfinalboy.backend.web.controllers;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import codurance.academyfinalboy.backend.actions.GetTeams;
+import codurance.academyfinalboy.backend.configurations.InterceptorConfiguration;
+import codurance.academyfinalboy.backend.infrastructure.services.GoogleTokenValidator;
+import codurance.academyfinalboy.backend.model.team.Team;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsoft.applicationinsights.TelemetryClient;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(GetTeamsController.class)
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+public class GetTeamsControllerShould {
+
+  @Autowired MockMvc mockMvc;
+  @Autowired ObjectMapper objectMapper;
+  @MockBean GetTeams getTeams;
+
+  @MockBean InterceptorConfiguration interceptorConfiguration;
+  @MockBean GoogleTokenValidator googleTokenValidator;
+  @MockBean TelemetryClient telemetryClient;
+
+  @Test
+  void get_a_list_of_teams_on_request() throws Exception {
+    var expectedTeam1 = new Team("Team 1", "Team 1 description", 3L);
+    var expectedTeam2 = new Team("Team 2", "Team 2 description", 3L);
+
+    expectedTeam1.setId(11L);
+    expectedTeam2.setId(14L);
+
+    var expectedTeams =
+        new GetTeamsController.GetTeamsResponse(List.of(expectedTeam1, expectedTeam2));
+    ObjectMapper mapper = new ObjectMapper();
+    String expectedJSON = mapper.writeValueAsString(expectedTeams);
+
+    when(getTeams.execute()).thenReturn(expectedTeams.teams());
+
+    mockMvc
+        .perform(get("/teams").contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().json(expectedJSON));
+  }
+}
