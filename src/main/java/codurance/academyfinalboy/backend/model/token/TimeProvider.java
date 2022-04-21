b07e50872f4bb72e11647377b4fb0e9ec49988b3
Brian Hoang <brian.hd.hoang@codurance.com>
2022-04-21 13:51:21 +0100

    implement time provider

--- src/main/java/codurance/academyfinalboy/backend/model/token/TimeProvider.java
+++ src/main/java/codurance/academyfinalboy/backend/model/token/TimeProvider.java
@@ -4,6 +4,6 @@ import java.time.LocalDateTime;
 
 public class TimeProvider {
     public LocalDateTime getCurrentTime() {
-        throw new UnsupportedOperationException();
+        return LocalDateTime.now();
     }
 }
