55dc191a4421e4d5093bb766e246b32403bcf43f
iagardi <garbigz@gmail.com>
2022-04-25 13:19:32 +0100

    cucumber test added

---
+++ src/test/java/cucumber/CreateActivityStepdefs.java
@@ -0,0 +1,9 @@
+package cucumber;
+
+import io.cucumber.java.en.Given;
+
+public class CreateActivityStepdefs {
+    @Given("the following users exist:")
+    public void theFollowingUsersExist() {
+    }
+}
