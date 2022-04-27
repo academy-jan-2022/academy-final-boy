4830430673d70258578bf05151c824d67c2099d0
Nicola <nicola_rushton@outlook.com>
2022-04-27 09:31:01 +0100

    refactor

--- src/test/java/codurance/academyfinalboy/backend/actions/JoinTeamShould.java
+++ src/test/java/codurance/academyfinalboy/backend/actions/JoinTeamShould.java
@@ -2,6 +2,7 @@ package codurance.academyfinalboy.backend.actions;
 
 import codurance.academyfinalboy.backend.model.token.TokenService;
 import codurance.academyfinalboy.backend.model.user.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -20,23 +21,23 @@ public class JoinTeamShould {
     @Mock
     private TokenService mockedTokenService;
 
+    private UUID joinTokenId;
+
+  @BeforeEach
+  void setUp() {
+      JoinTeam joinTeamAction = new JoinTeam(mockedUserService, mockedTokenService);
+      joinTokenId = UUID.randomUUID();
+
+      joinTeamAction.execute(joinTokenId);
+  }
+
     @Test void
     get_the_current_user() {
-        JoinTeam joinTeamAction = new JoinTeam(mockedUserService, mockedTokenService);
-        UUID joinTokenId = UUID.randomUUID();
-
-        joinTeamAction.execute(joinTokenId);
-
         verify(mockedUserService).getCurrentUser();
     }
 
     @Test void
     call_the_token_service_to_validate_the_token() {
-        JoinTeam joinTeamAction = new JoinTeam(mockedUserService, mockedTokenService);
-        UUID joinTokenId = UUID.randomUUID();
-
-        joinTeamAction.execute(joinTokenId);
-
         verify(mockedTokenService).getToken(joinTokenId);
     }
 
