4c8739bb1a4de7314cfddf81eaf7ae5d130c5e4d
Sofia Carballo <sofia.carballo@codurance.com>
2022-04-28 15:39:25 +0200

    Refactor rest exception handler

--- src/main/java/codurance/academyfinalboy/backend/web/controllers/RestExceptionHandler.java
+++ src/main/java/codurance/academyfinalboy/backend/web/controllers/RestExceptionHandler.java
@@ -1,6 +1,11 @@
 package codurance.academyfinalboy.backend.web.controllers;
 
+import codurance.academyfinalboy.backend.actions.InvalidUserException;
+import codurance.academyfinalboy.backend.model.team.UserNotMemberOfTeamException;
+import codurance.academyfinalboy.backend.model.token.InvalidTokenException;
 import com.microsoft.applicationinsights.TelemetryClient;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,16 +15,27 @@ import org.springframework.web.context.request.WebRequest;
 public class RestExceptionHandler {
 
   private final TelemetryClient telemetryClient;
+  private final Map<Class<? extends Exception>, HttpStatus> exceptionToStatusMap;
 
   public RestExceptionHandler(TelemetryClient telemetryClient) {
     this.telemetryClient = telemetryClient;
+    this.exceptionToStatusMap =
+        Map.of(
+            InvalidUserException.class,
+            HttpStatus.UNAUTHORIZED,
+            UserNotMemberOfTeamException.class,
+            HttpStatus.FORBIDDEN,
+            InvalidTokenException.class,
+            HttpStatus.NOT_FOUND);
   }
 
   @ExceptionHandler(value = {Exception.class})
   public ResponseEntity<Object> handleIllegalState(Exception ex, WebRequest request) {
     telemetryClient.trackException(ex);
 
-    return ResponseEntity.badRequest().body(new ErrorResponse(ex.getMessage()));
+    HttpStatus status = exceptionToStatusMap.getOrDefault(ex.getClass(), HttpStatus.BAD_REQUEST);
+
+    return ResponseEntity.status(status).body(new ErrorResponse(ex.getMessage()));
   }
 
   private record ErrorResponse(String message) {}
