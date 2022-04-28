8136c77830acd75657cd3a862279bb93d8a4b159
Brian Hoang <brian.hd.hoang@codurance.com>
2022-04-28 15:14:09 +0100

    green

--- src/main/java/codurance/academyfinalboy/backend/actions/RemoveUser.java
+++ src/main/java/codurance/academyfinalboy/backend/actions/RemoveUser.java
@@ -1,17 +1,21 @@
 package codurance.academyfinalboy.backend.actions;
 
 import codurance.academyfinalboy.backend.model.team.TeamService;
+import codurance.academyfinalboy.backend.model.user.UserService;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RemoveUser {
+    private UserService userService;
     private TeamService teamService;
 
-    public RemoveUser(TeamService teamService) {
+    public RemoveUser(UserService userService, TeamService teamService) {
+        this.userService = userService;
         this.teamService = teamService;
     }
 
     public void execute(Long teamId) {
-        throw new UnsupportedOperationException();
+        var userId = userService.getCurrentUser().orElseThrow().getId();
+        teamService.removeUserFromTeam(userId, teamId);
     }
 }
--- src/test/java/codurance/academyfinalboy/backend/actions/RemoveUserShould.java
+++ src/test/java/codurance/academyfinalboy/backend/actions/RemoveUserShould.java
@@ -1,10 +1,14 @@
 package codurance.academyfinalboy.backend.actions;
 
+import codurance.academyfinalboy.backend.builders.UserBuilder;
 import codurance.academyfinalboy.backend.model.team.TeamService;
+import codurance.academyfinalboy.backend.model.user.User;
+import codurance.academyfinalboy.backend.model.user.UserService;
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
 
 public class RemoveUserShould {
     public static final long TEAM_ID = 1L;
@@ -13,7 +17,13 @@ public class RemoveUserShould {
   @Test
   void call_the_team_service_to_remove_the_user_from_a_team() {
       var mockedTeamService = mock(TeamService.class);
-      RemoveUser removeUser = new RemoveUser(mockedTeamService);
+      var mockedUserService = mock(UserService.class);
+      RemoveUser removeUser = new RemoveUser(mockedUserService, mockedTeamService);
+
+      User user = new UserBuilder().id(USER_ID).build();
+
+      when(mockedUserService.getCurrentUser()).thenReturn(Optional.of(user));
+
       removeUser.execute(TEAM_ID);
       verify(mockedTeamService).removeUserFromTeam(USER_ID, TEAM_ID);
   }
