58c422f1810b0f21622e3b57dc425c53ec77a0af
Nicola <nicola_rushton@outlook.com>
2022-04-28 16:22:37 +0100

    green: invalid token exception for rest handler

--- src/test/java/codurance/academyfinalboy/backend/web/controllers/RestExceptionHandlerShould.java
+++ src/test/java/codurance/academyfinalboy/backend/web/controllers/RestExceptionHandlerShould.java
@@ -3,6 +3,7 @@ package codurance.academyfinalboy.backend.web.controllers;
 import codurance.academyfinalboy.backend.BaseSpringTest;
 import codurance.academyfinalboy.backend.actions.InvalidUserException;
 import codurance.academyfinalboy.backend.model.team.UserNotMemberOfTeamException;
+import codurance.academyfinalboy.backend.model.token.InvalidTokenException;
 import com.microsoft.applicationinsights.TelemetryClient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -40,6 +41,16 @@ public class RestExceptionHandlerShould extends BaseSpringTest {
     return_FORBIDDEN_status_on_invalid_user_exception(){
         HttpStatus expectedResponseStatus = HttpStatus.FORBIDDEN;
         UserNotMemberOfTeamException exception = new UserNotMemberOfTeamException();
+
+        var response = restExceptionHandler.handleIllegalState(exception, request);
+
+        assertThat(response.getStatusCode(), equalTo(expectedResponseStatus));
+    }
+
+    @Test void
+    return_NOT_FOUND_status_on_invalid_user_exception(){
+        HttpStatus expectedResponseStatus = HttpStatus.NOT_FOUND;
+        InvalidTokenException exception = new InvalidTokenException("Invalid token");
 
         var response = restExceptionHandler.handleIllegalState(exception, request);
 
