4390c2f3a95dc8949a79329cc15eca6ad5da66bd
Brian Hoang <brian.hd.hoang@codurance.com>
2022-04-28 15:05:27 +0100

    green

--- src/main/java/codurance/academyfinalboy/backend/web/controllers/RemoveUserController.java
+++ src/main/java/codurance/academyfinalboy/backend/web/controllers/RemoveUserController.java
@@ -1,7 +1,20 @@
 package codurance.academyfinalboy.backend.web.controllers;
 
+import codurance.academyfinalboy.backend.actions.RemoveUser;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RemoveUserController {
+    private final RemoveUser removeUser;
+
+    public RemoveUserController(RemoveUser removeUser) {
+        this.removeUser = removeUser;
+    }
+
+    @DeleteMapping("/remove-user")
+    void removeUser(@RequestParam Long teamId) {
+        removeUser.execute(teamId);
+    }
 }
